929be4347f6a81253d8545c9ab0bdd69802fdf40
irfan farras <irfanfarras10@gmail.com>
2021-03-29 16:13:10 +0700

    initial commit

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import com.google.cloud.spring.pubsub.core.PubSubTemplate;
+import com.google.cloud.spring.pubsub.integration.outbound.PubSubMessageHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+    
+    @MessagingGateway(defaultRequestChannel = "pubsubOutputChannel")
+    public interface PubsubOutboundGateway {
+        void sendToPubsub(String text);
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "pubsubOutputChannel")
+    public MessageHandler messageSender(PubSubTemplate pubsubTemplate) {
+        return new PubSubMessageHandler(pubsubTemplate, "exampleTopic");
+    }
+
+    @Autowired
+    private PubsubOutboundGateway messagingGateway;
+
+    @PostMapping("/postMessage")
+    public RedirectView postMessage(@RequestParam("message") String message) {
+        this.messagingGateway.sendToPubsub(message);
+        return new RedirectView("/");
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+
+}
